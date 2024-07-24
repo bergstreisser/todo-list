82646c569091c38179033719f4c6a5a454b63ba6
Bergi <info@bergstreisser.de>
2024-07-24 14:10:19 +0200

    todo list

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { TodoWrapper } from './componets/TodoWrapper';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoWrapper />
+    </div>
+  );
+}
+
+export default App;
